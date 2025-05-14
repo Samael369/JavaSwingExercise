deb0a604d2992e564bbb9642737bf8dbd85547e5
alireza <alireza10abasi@gmail.com>
2025-05-14 22:15:42 +0330

    updated textField

--- part1/app/ButtonPanel.java
+++ part1/app/ButtonPanel.java
@@ -1,17 +1,30 @@
 package part1.app;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ButtonPanel extends JPanel {
-    private final String[] buttons = {
-            "1", "2", "3", "/",
-            "4", "5", "6", "*",
-            "7", "8", "9", "-",
-            ".", "0", "=", "+",
-            "C", "(", ")", "b"
+    private final String[][] buttons = {
+            { "1", "2", "3", "/" },
+            { "4", "5", "6", "*" },
+            { "7", "8", "9", "-" },
+            { ".", "0", "=", "+" },
+            { "C", "(", ")", "b" }
     };
 
     public ButtonPanel(DisplayPanel display) {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 4; col++) {
+                gbc.gridx = row;
+                gbc.gridy = col;
+                add(new JButton(buttons[row][col]), gbc);
+            }
+        }
     }
 }
--- part1/app/DisplayPanel.java
+++ part1/app/DisplayPanel.java
@@ -1,11 +1,29 @@
 package part1.app;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class DisplayPanel extends JPanel {
+    public JTextField textField = new JTextField();
+
     public DisplayPanel() {
         setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(280, 70));
+        setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        textField.setFont(new Font("Arial", Font.PLAIN, 32));
+        textField.setHorizontalAlignment(JTextField.LEFT);
+    }
+
+    public String getExpression() {
+        return textField.getText();
+    }
+
+    public void setExpression(String expression) {
+        textField.setText(expression);
     }
 }
